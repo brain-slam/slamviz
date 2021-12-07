b99afc4f5f7ffc6935e560ce4a94ec693249d12f
pron.a <alexandre.pron@gmaiL.com>
2021-12-07 15:17:03 +0100

    [ENH] creating setup.py to handle slam package dependency

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+BASE_REQUIREMENTS = ["brain-slam", "visbrain"]
+TEST_REQUIREMENTS = ["flake8", "autopep8", "pytest", "pytest-cov", "coveralls"]
+
+setup(
+    name="slamviz",
+    version="0.0.0",
+    packages=find_packages(),
+    author="The MeCA Team",
+    description="Surface anaLysis And Modeling",
+    url="https://github.com/brain-slam/slamviz",
+    license="MIT",
+    python_requires=">=3.8",  # enforce Python 3.6 as minimum
+    install_requires=BASE_REQUIREMENTS,
+    extras_require={"dev": TEST_REQUIREMENTS},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
