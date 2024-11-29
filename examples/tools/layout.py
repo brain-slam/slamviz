9e737d753fbd60df4dd45d05f09aa85c8018826f
leroux.b <baptiste.leroux@univ-amu.fr>
2024-11-29 12:27:39 +0100

    New viz with upload buttons

--- examples/tools/layout.py
+++ examples/tools/layout.py
@@ -9,17 +9,16 @@ from .functions import load_mesh, read_gii_file, create_slider_marks, get_colors
 def create_layout(mesh_path, texture_paths=None):
     # Charger le mesh
     mesh = load_mesh(mesh_path)
+
     vertices = mesh.vertices
     faces = mesh.faces
 
     # Charger la texture (si fournie)
-    # scalars = read_gii_file(texture_path) if texture_path else None
-    print("before calling: ", texture_paths[0])
     scalars = read_gii_file(texture_paths[0]) if texture_paths else None
-
 
     # Définir l'intervalle min et max par défaut des scalaires si disponibles
     color_min_default, color_max_default = (np.min(scalars), np.max(scalars)) if scalars is not None else (0, 1)
+
 
     layout = html.Div([
         html.H1("Visualisation de maillage 3D avec color bar interactive", style={'textAlign': 'center'}),
@@ -31,15 +30,33 @@ def create_layout(mesh_path, texture_paths=None):
                 value=texture_paths[0],
                 clearable=False,
                 style={
-                    'width': '300px',
+                    'width': '50%',
+                    'text-align': 'center',
+                    'display': 'inline-block',
                 }
             ),
         ], style={
-                'display': 'flex',
-                'flexDirection': 'column',
-                'alignItems': 'center',
-                'justifyContent': 'center',
-            }),
+            'display': 'flex',
+            'flexDirection': 'column',
+            'alignItems': 'center',
+            'justifyContent': 'center',
+        }),
+        html.Div([
+            dcc.Upload(
+                id="upload-file-button",
+                children=html.Button(
+                    "Sélectionner un fichier",
+                    id="upload-button",
+                ),
+                multiple=True,
+                style={
+                    "margin-top": "2%",
+                    "width": "100%",
+                    "text-align": "center",
+                    "display": "inline-block"
+                }
+            )
+        ]),
         html.Div([
 
             # Texture selection dropdown
@@ -91,10 +108,10 @@ def create_layout(mesh_path, texture_paths=None):
                     value=[color_min_default, color_max_default],
                     marks=create_slider_marks(color_min_default, color_max_default),
                     vertical=True,
-                    verticalHeight=640,
+                    verticalHeight=500,
                     tooltip={"placement": "right", "always_visible": True}
                 )
-            ], style={'height': '640px', 'display': 'inline-block', 'margin-right': '10px'}),
+            ], style={'display': 'inline-block'}),
             html.Div([
                 dcc.Graph(id='3d-mesh')
             ], style={'display': 'inline-block', 'verticalAlign': 'top', 'textAlign': 'center'}),
