11586d58b6940c3fbf4c7ad2d05a02cbc32618d8
aeon00 <harveydienye@gmail.com>
2024-11-29 12:14:22 +0100

    Visualization for example_differential_geometry

--- examples/example_differential_geometry.py
+++ examples/example_differential_geometry.py
@@ -16,19 +16,26 @@ example of differential geometry tools in slam
 # importation of slam modules
 import slam.io as sio
 import slam.differential_geometry as sdg
-import slam.plot as splt
+import slam.texture as stex
+from tools import app
 
 ###############################################################################
 # loading an examplar mesh and corresponding texture and show it
-mesh_file = '../examples/data/example_mesh.gii'
-texture_file = '../examples/data/example_texture.gii'
+mesh_file = 'examples/data/example_mesh.gii'
+texture_file = 'examples/data/example_texture.gii'
+
+s_mesh_path = 'examples/data/s_mesh.gii'
+norm_grad_path = 'examples/data/norm_grad.gii'
+dpf_path = 'examples/data/dpf.gii'
+
 mesh = sio.load_mesh(mesh_file)
 tex = sio.load_texture(texture_file)
 
-visb_sc = splt.visbrain_plot(mesh=mesh, tex=tex.darray[0],
-                             caption='mesh with curvature',
-                             cblabel='curvature')
-visb_sc.preview()
+# visb_sc = splt.visbrain_plot(mesh=mesh, tex=tex.darray[0],
+#                              caption='mesh with curvature',
+#                              cblabel='curvature')
+# visb_sc.preview()
+
 
 ###############################################################################
 # compute various types of Laplacian of the mesh
@@ -42,11 +49,11 @@ lap, lap_b = sdg.compute_mesh_laplacian(mesh, lap_type='authalic')
 ###############################################################################
 # smooth the mesh using Laplacian
 s_mesh = sdg.laplacian_mesh_smoothing(mesh, nb_iter=100, dt=0.1)
-
+sio.write_mesh(s_mesh, s_mesh_path)
 ###############################################################################
 # show it
-visb_sc = splt.visbrain_plot(mesh=s_mesh, caption='smoothed mesh')
-visb_sc.preview()
+# visb_sc = splt.visbrain_plot(mesh=s_mesh, caption='smoothed mesh')
+# visb_sc.preview()
 
 ###############################################################################
 # compute the gradient of texture tex
@@ -56,21 +63,24 @@ grad = sdg.gradient(mesh, tex.darray[0])
 print(grad)
 norm_grad = sdg.norm_gradient(mesh, tex.darray[0])
 print(norm_grad)
-
+norm_grad_tex = stex.TextureND(norm_grad)
+sio.write_texture(norm_grad_tex, norm_grad_path)
 ###############################################################################
 # show it
-visb_sc = splt.visbrain_plot(mesh=mesh, tex=norm_grad,
-                             caption='norm of the gradient of curvature',
-                             cblabel='gradient magnitude')
-visb_sc.preview()
+# visb_sc = splt.visbrain_plot(mesh=mesh, tex=norm_grad,
+#                              caption='norm of the gradient of curvature',
+#                              cblabel='gradient magnitude')
+# visb_sc.preview()
 
 ###############################################################################
 # compute the depth potential function
 dpf = sdg.depth_potential_function(mesh, tex.darray[0], [0.3])
-
+dpf_tex = stex.TextureND(dpf[0])
+sio.write_texture(dpf_tex, dpf_path)
 ###############################################################################
 # show it
-visb_sc = splt.visbrain_plot(mesh=mesh, tex=dpf[0],
-                             caption='depth potential function',
-                             cblabel='dpf')
-visb_sc.preview()
+# visb_sc = splt.visbrain_plot(mesh=mesh, tex=dpf[0],
+#                              caption='depth potential function',
+#                              cblabel='dpf')
+# visb_sc.preview()
+# app.run_dash_app(mesh_file, texture_paths=[dpf_path, norm_grad_path, texture_file])
