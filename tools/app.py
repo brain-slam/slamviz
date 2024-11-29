3b48c5364e102ca0e2763ceba43bb943d6285268
leroux.b <baptiste.leroux@univ-amu.fr>
2024-11-29 16:26:04 +0100

    We can start the app with 2 opts: empty or not

--- tools/app.py
+++ tools/app.py
@@ -1,11 +1,14 @@
 import dash
 import argparse
-from .layout import create_layout
-from .callbacks import register_callbacks
-
+try:
+    from layout import create_layout
+    from callbacks import register_callbacks
+except:
+    from .layout import create_layout
+    from .callbacks import register_callbacks
 
 # Fonction principale pour exécuter l'application
-def run_dash_app(mesh_path, texture_paths=None, port=8050):
+def run_dash_app(mesh_path=None, texture_paths=None):
     # Créer l'application Dash
     app = dash.Dash(__name__)
 
@@ -16,6 +19,8 @@ def run_dash_app(mesh_path, texture_paths=None, port=8050):
     register_callbacks(app, mesh_path, texture_paths)
 
     # Lancer l'application
-    app.run_server(debug=True, port=port)
+    app.run_server(debug=True)
 
 
+if __name__ == "__main__":
+    run_dash_app()
--- tools/callbacks.py
+++ tools/callbacks.py
@@ -1,16 +1,21 @@
 import dash
 from dash.dependencies import Input, Output, State
-from .functions import load_mesh, read_gii_file, plot_mesh_with_colorbar, get_colorscale_names, create_slider_marks
 import numpy as np
 import os
 import base64
+try:
+    from functions import load_mesh, read_gii_file, plot_mesh_with_colorbar, get_colorscale_names, create_slider_marks
+except:
+    from .functions import load_mesh, read_gii_file, plot_mesh_with_colorbar, get_colorscale_names, create_slider_marks
 
 
 def register_callbacks(app, mesh_path, texture_paths):
-    # Charger le mesh
-    mesh = load_mesh(mesh_path)
-    vertices = mesh.vertices
-    faces = mesh.faces
+    # Charger le mesh initial
+    if mesh_path is not None:
+        print("here")
+        mesh = load_mesh(mesh_path)
+        vertices = mesh.vertices
+        faces = mesh.faces
 
     @app.callback(
         [Output('3d-mesh', 'figure'),
@@ -23,42 +28,100 @@ def register_callbacks(app, mesh_path, texture_paths):
          Input('toggle-contours', 'value'),
          Input('toggle-black-intervals', 'value'),
          Input('colormap-dropdown', 'value'),
-         Input('toggle-center-colormap', 'value')],
-        [State('3d-mesh', 'relayoutData')]
+         Input('toggle-center-colormap', 'value'),
+         Input('upload-mesh-button', 'contents')],
+        [State('upload-mesh-button', 'filename'),
+         State('3d-mesh', 'relayoutData')]
     )
-    def update_figure(selected_texture_path, value_range, toggle_contours, toggle_black_intervals,
-                      selected_colormap, center_colormap, relayout_data):
-        # Charger la texture sélectionnée
-        scalars = read_gii_file(selected_texture_path) if selected_texture_path else None
+    def update_figure_combined(selected_texture_path, value_range, toggle_contours, toggle_black_intervals,
+                               selected_colormap, center_colormap, uploaded_contents, uploaded_filename, relayout_data):
+        nonlocal mesh_path, vertices, faces
 
-        # Calculer les min et max des scalaires
-        color_min_default, color_max_default = (np.min(scalars), np.max(scalars)) if scalars is not None else (0, 1)
+        # Si un mesh est uploadé, le charger
+        if uploaded_contents is not None:
+            content_type, content_string = uploaded_contents.split(',')
+            decoded_data = base64.b64decode(content_string)
 
-        # Gérer les paramètres de la vue
-        min_value, max_value = value_range
-        camera = relayout_data['scene.camera'] if relayout_data and 'scene.camera' in relayout_data else None
-        show_contours = 'on' in toggle_contours
-        use_black_intervals = 'on' in toggle_black_intervals
-        center_on_zero = 'on' in center_colormap
+            # Sauvegarder temporairement le fichier pour le charger
+            upload_folder = "examples/data"
+            mesh_path = os.path.join(upload_folder, uploaded_filename)
 
-        # Si centrer sur zéro est activé, ajuster la plage de couleurs
-        if center_on_zero and scalars is not None:
-            max_val = max(abs(color_min_default), abs(color_max_default))
-            color_min_default, color_max_default = -max_val, max_val
+            with open(mesh_path, "wb") as f:
+                f.write(decoded_data)
 
-        # Créer la figure
-        fig = plot_mesh_with_colorbar(
-            vertices, faces, scalars,
-            color_min=color_min_default, color_max=color_max_default,
-            camera=camera, show_contours=show_contours,
-            colormap=selected_colormap,
-            use_black_intervals=use_black_intervals,
-            center_colormap_on_zero=center_on_zero
-        )
+            # Charger le nouveau mesh
+            mesh = load_mesh(mesh_path)
+            vertices, faces = mesh.vertices, mesh.faces
 
-        return fig, color_min_default, color_max_default, [color_min_default, color_max_default], create_slider_marks(
-            color_min_default, color_max_default)
+            # Charger la texture sélectionnée
+            scalars = read_gii_file(selected_texture_path) if selected_texture_path else None
 
+            # Calculer les min et max des scalaires
+            color_min_default, color_max_default = (np.min(scalars), np.max(scalars)) if scalars is not None else (0, 1)
+
+            # Gérer les paramètres de la vue
+            min_value, max_value = value_range
+            camera = relayout_data['scene.camera'] if relayout_data and 'scene.camera' in relayout_data else None
+            show_contours = 'on' in toggle_contours
+            use_black_intervals = 'on' in toggle_black_intervals
+            center_on_zero = 'on' in center_colormap
+
+            # Si centrer sur zéro est activé, ajuster la plage de couleurs
+            if center_on_zero and scalars is not None:
+                max_val = max(abs(color_min_default), abs(color_max_default))
+                color_min_default, color_max_default = -max_val, max_val
+
+            # Créer la figure
+            fig = plot_mesh_with_colorbar(
+                vertices, faces, scalars,
+                color_min=color_min_default, color_max=color_max_default,
+                camera=camera, show_contours=show_contours,
+                colormap=selected_colormap,
+                use_black_intervals=use_black_intervals,
+                center_colormap_on_zero=center_on_zero
+            )
+
+            return fig, color_min_default, color_max_default, [color_min_default, color_max_default], create_slider_marks(
+                color_min_default, color_max_default)
+        else:
+            if mesh_path is not None:
+                mesh = load_mesh(mesh_path)
+                vertices = mesh.vertices
+                faces = mesh.faces
+
+                scalars = read_gii_file(selected_texture_path) if selected_texture_path else None
+
+                # Calculer les min et max des scalaires
+                color_min_default, color_max_default = (np.min(scalars), np.max(scalars)) if scalars is not None else (
+                0, 1)
+
+                # Gérer les paramètres de la vue
+                min_value, max_value = value_range
+                camera = relayout_data['scene.camera'] if relayout_data and 'scene.camera' in relayout_data else None
+                show_contours = 'on' in toggle_contours
+                use_black_intervals = 'on' in toggle_black_intervals
+                center_on_zero = 'on' in center_colormap
+
+                # Si centrer sur zéro est activé, ajuster la plage de couleurs
+                if center_on_zero and scalars is not None:
+                    max_val = max(abs(color_min_default), abs(color_max_default))
+                    color_min_default, color_max_default = -max_val, max_val
+
+                # Créer la figure
+                fig = plot_mesh_with_colorbar(
+                    vertices, faces, scalars,
+                    color_min=color_min_default, color_max=color_max_default,
+                    camera=camera, show_contours=show_contours,
+                    colormap=selected_colormap,
+                    use_black_intervals=use_black_intervals,
+                    center_colormap_on_zero=center_on_zero
+                )
+
+                return fig, color_min_default, color_max_default, [color_min_default,
+                                                                   color_max_default], create_slider_marks(
+                    color_min_default, color_max_default)
+            else:
+                return dash.no_update
     # Callback pour mettre à jour la liste des colormaps en fonction du type choisi
     @app.callback(
         Output('colormap-dropdown', 'options'),
@@ -80,9 +143,11 @@ def register_callbacks(app, mesh_path, texture_paths):
         ],
         prevent_initial_call=True
     )
-    def update_dropdown_on_upload(file_contents, file_names):
+    def update_texture_dropdown(file_contents, file_names):
         nonlocal texture_paths
+        print("here")
         if file_contents is not None:
+            texture_paths = []
             uploaded_paths = []
             upload_folder = "examples/data"
 
@@ -99,7 +164,7 @@ def register_callbacks(app, mesh_path, texture_paths):
             texture_paths.extend(uploaded_paths)
 
             dropdown_options = [{'label': os.path.basename(path), 'value': path} for path in texture_paths]
-            default_value = uploaded_paths[0] if len(uploaded_paths) == 1 else uploaded_paths
+            default_value = uploaded_paths[0] if uploaded_paths else None
 
             return dropdown_options, default_value
 
